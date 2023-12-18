c52f9c0f43dd2cb06175f938f017ea8713e9fccd
Brien-Austin <brienaustinclayton@gmail.com>
2023-12-18 21:18:43 +0530

    Added neccessary modules and initialized

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStore } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDhczrRSExN2O_bM0Gq8qdish8QzRV1ySQ",
   authDomain: "image-storage-973de.firebaseapp.com",
@@ -16,6 +12,6 @@ const firebaseConfig = {
   measurementId: "G-KJV16DWK64",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const imageStorage = getStore(app);
+const urlStorage = getFirestore(app);
